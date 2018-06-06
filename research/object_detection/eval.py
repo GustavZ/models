9ea3088cc5b7ae7e26e2a523eb2bfe6f04a919ea
GustavZ <gustav.zitzewitz@gmx.de>
2018-06-06 09:50:34 +0200

    added Option to eval only on CPU or on all checkpoints

--- research/object_detection/eval.py
+++ research/object_detection/eval.py
@@ -49,7 +49,6 @@ import tensorflow as tf
 
 from object_detection import evaluator
 from object_detection.builders import dataset_builder
-from object_detection.builders import graph_rewriter_builder
 from object_detection.builders import model_builder
 from object_detection.utils import config_util
 from object_detection.utils import dataset_util
@@ -64,6 +63,9 @@ flags.DEFINE_boolean('eval_training_data', False,
 flags.DEFINE_string('checkpoint_dir', '',
                     'Directory containing checkpoints to evaluate, typically '
                     'set to `train_dir` used in the training job.')
+flags.DEFINE_boolean('evaluate_all_checkpoints',False,'Whether to evaluate all the checkpoints in the directory in order, \
+                     useful for evaluating all checkpoints after training \
+                     setting this to True will override max_eval and time between evaluations. ')
 flags.DEFINE_string('eval_dir', '',
                     'Directory to write eval summaries to.')
 flags.DEFINE_string('pipeline_config_path', '',
@@ -78,14 +80,16 @@ flags.DEFINE_string('model_config_path', '',
 flags.DEFINE_boolean('run_once', False, 'Option to only run a single pass of '
                      'evaluation. Overrides the `max_evals` parameter in the '
                      'provided config.')
+flags.DEFINE_boolean('run_on_CPU_only', False, 'Option to run eval only on CPU, set this to True \
+                     if you want to train on GPU and evaluate on CPU')
 FLAGS = flags.FLAGS
-
-os.environ['CUDA_VISIBLE_DEVICES'] = '-1' # ADDED BY GUSTAV
 
 
 def main(unused_argv):
   assert FLAGS.checkpoint_dir, '`checkpoint_dir` is missing.'
   assert FLAGS.eval_dir, '`eval_dir` is missing.'
+  if FLAGS.run_on_CPU_only == True:
+    os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
   tf.gfile.MakeDirs(FLAGS.eval_dir)
   if FLAGS.pipeline_config_path:
     configs = config_util.get_configs_from_pipeline_file(
@@ -130,19 +134,8 @@ def main(unused_argv):
   if FLAGS.run_once:
     eval_config.max_evals = 1
 
-  graph_rewriter_fn = None
-  if 'graph_rewriter_config' in configs:
-    graph_rewriter_fn = graph_rewriter_builder.build(
-        configs['graph_rewriter_config'], is_training=False)
-
-  evaluator.evaluate(
-      create_input_dict_fn,
-      model_fn,
-      eval_config,
-      categories,
-      FLAGS.checkpoint_dir,
-      FLAGS.eval_dir,
-      graph_hook_fn=graph_rewriter_fn)
+  evaluator.evaluate(create_input_dict_fn, model_fn, eval_config, categories,
+                     FLAGS.checkpoint_dir, FLAGS.eval_dir, FLAGS.evaluate_all_checkpoints)
 
 
 if __name__ == '__main__':
